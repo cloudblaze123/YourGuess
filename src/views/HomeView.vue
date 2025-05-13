85015d885eba8894da4a83b58e43d112c13d89d6
cloudblaze123 <cloudblaze123@gmail.com>
2025-05-13 10:53:00 +0800

    新增【统计页面】

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,6 +10,11 @@
         <div class="tab-content border-base-300 bg-base-100 p-10">
             <TrainView />
         </div>
+
+        <input type="radio" name="my_tabs_2" class="tab" aria-label="Statistics" />
+        <div class="tab-content border-base-300 bg-base-100 p-10">
+            <StatisticsView />
+        </div>
     </div>
 </template>
 
@@ -17,4 +22,5 @@
 <script setup lang="ts">
 import GameView from './GameView.vue';
 import TrainView from './TrainView.vue';
+import StatisticsView from './StatisticsView.vue';
 </script>
---
+++ src/views/StatisticsView.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>统计页面</div>
+</template>
+
+
+<script setup lang="ts">
+
+</script>
