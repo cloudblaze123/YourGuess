e406afd08281eea89805ab1abbeee97394abb886
cloudblaze123 <cloudblaze123@gmail.com>
2025-05-13 12:55:09 +0800

    安装mathjs

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,8 @@
 <template>
+    <div>Hello Mathjs</div>
+    <div>{{ matrixA }} + {{ matrixB }}</div>
+    <div>result: {{ result }}</div>
+
     <header class="text-3xl font-bold">YourGuess</header>
     <RouterView />
 </template>
@@ -6,4 +10,10 @@
 
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+
+import { matrix, add } from 'mathjs';
+
+const matrixA = matrix([[1, 2], [3, 4]]);
+const matrixB = matrix([[5, 6], [7, 8]]);
+const result = add(matrixA, matrixB); // [[19, 22], [43, 50]]
 </script>
