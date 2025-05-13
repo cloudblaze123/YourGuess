adbdc549266af31c128d9071ef12435f72cfb273
cloudblaze123 <cloudblaze123@gmail.com>
2025-05-13 12:31:55 +0800

    新增【游戏设置】组件，可调整目标数字的生成范围

---
+++ src/components/GameSetting.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="flex flex-col">
+        <div class="text-xl">Setting</div>
+        <form @submit.prevent="updateSetting">
+            <div class="flex mt-2">
+                <label for="min">Min:</label>
+                <input type="number" v-model="min">
+                <label for="max">Max:</label>
+                <input type="number" v-model="max">
+            </div>
+            <button class="btn btn-primary mt-2">Confirm</button>
+        </form>
+    </div>
+</template>
+
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+import { game } from '@/game/gameProvider'
+
+
+const min = ref(game.min)
+const max = ref(game.max)
+
+
+function updateSetting() {
+    game.min = min.value
+    game.max = max.value
+}
+
+</script>
--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,10 +1,14 @@
 <template>
     <div class="flex flex-col">
+        <button class="btn btn-primary mb-4" @click="resetGame">重置游戏</button>
+        <div v-if="debug">target: {{ target }}</div>
         <GuessHistory :guesses="guessHistory" :results="resultHistory" class="h-64 overflow-auto mb-4" />
         <form @submit.prevent="handleGuess">
             <input type="number" v-model="guess">
             <button class="btn btn-primary">Guess</button>
         </form>
+
+        <GameSettingView class="mt-4" />
     </div>
 </template>
 
@@ -12,12 +16,17 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import GuessHistory from '@/components/GuessHistory.vue';
+import GameSettingView from '@/components/GameSetting.vue';
 
-import { Result, Game } from '@/game/game'
+import { Result } from '@/game/game'
+import { game } from '@/game/gameProvider'
 
+const debug = true
 
-const game = new Game()
 game.initGame()
+
+const target = ref<number>(game.targetNumber)
+
 
 
 const result = ref(Result.CORRECT)
@@ -33,6 +42,15 @@ function handleGuess() {
 
     guessHistory.value.push(guess.value)
     resultHistory.value.push(result.value)
+}
+
+
+function resetGame() {
+    game.initGame()
+    target.value = game.targetNumber
+    guess.value = 0
+    guessHistory.value.splice(0, guessHistory.value.length)
+    resultHistory.value.splice(0, resultHistory.value.length)
 }
 
 </script>
