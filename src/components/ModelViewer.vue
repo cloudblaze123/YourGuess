b4c9b52ac09cfbb49566a7628a4e44d74ddf6288
cloudblaze123 <cloudblaze123@gmail.com>
2025-05-15 10:21:51 +0800

    新增【模型查看器】组件

---
+++ src/components/ModelViewer.vue
@@ -0,0 +1,24 @@
+<template>
+    模型查看器
+    <div class="flex flex-col">
+        <table class="border-collapse border-gray-300">
+            <tr v-for="(row, index) in weights">
+                <td v-for="(col, colIndex) in row" class="border-2 border-gray-300">
+                    {{ col }}
+                </td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const weights = ref([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [10, 11, 12]
+])
+</script>
--- src/views/ModelView.vue
+++ src/views/ModelView.vue
@@ -1,8 +1,9 @@
 <template>
     <div>查看模型</div>
+    <ModelViewer />
 </template>
 
 
 <script setup lang="ts">
-
+import ModelViewer from '@/components/ModelViewer.vue';
 </script>
