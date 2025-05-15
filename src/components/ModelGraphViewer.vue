79809682402731555fad7a063365211697a895ac
cloudblaze123 <cloudblaze123@gmail.com>
2025-05-15 10:54:38 +0800

    提取关系图的 data 、links 字段到同名变量中

--- src/components/ModelGraphViewer.vue
+++ src/components/ModelGraphViewer.vue
@@ -12,6 +12,72 @@ const chartRef = ref<HTMLDivElement | null>(null);
 let chartInstance: echarts.ECharts | null = null;
 
 
+// 定义数据
+const data = [
+    {
+        name: 'Node 1',
+        x: 300,
+        y: 300
+    },
+    {
+        name: 'Node 2',
+        x: 800,
+        y: 300
+    },
+    {
+        name: 'Node 3',
+        x: 550,
+        y: 100
+    },
+    {
+        name: 'Node 4',
+        x: 550,
+        y: 500
+    }
+]
+
+const links = [
+    {
+        source: 0,
+        target: 1,
+        symbolSize: [5, 20],
+        label: {
+            show: true
+        },
+        lineStyle: {
+            width: 5,
+            curveness: 0.2
+        }
+    },
+    {
+        source: 'Node 2',
+        target: 'Node 1',
+        label: {
+            show: true
+        },
+        lineStyle: {
+            curveness: 0.2
+        }
+    },
+    {
+        source: 'Node 1',
+        target: 'Node 3'
+    },
+    {
+        source: 'Node 2',
+        target: 'Node 3'
+    },
+    {
+        source: 'Node 2',
+        target: 'Node 4'
+    },
+    {
+        source: 'Node 1',
+        target: 'Node 4'
+    }
+]
+
+
 
 
 type EChartsOption = echarts.EChartsOption;
@@ -41,69 +107,8 @@ function initChart() {
                 edgeLabel: {
                     fontSize: 20
                 },
-                data: [
-                    {
-                        name: 'Node 1',
-                        x: 300,
-                        y: 300
-                    },
-                    {
-                        name: 'Node 2',
-                        x: 800,
-                        y: 300
-                    },
-                    {
-                        name: 'Node 3',
-                        x: 550,
-                        y: 100
-                    },
-                    {
-                        name: 'Node 4',
-                        x: 550,
-                        y: 500
-                    }
-                ],
-                // links: [],
-                links: [
-                    {
-                        source: 0,
-                        target: 1,
-                        symbolSize: [5, 20],
-                        label: {
-                            show: true
-                        },
-                        lineStyle: {
-                            width: 5,
-                            curveness: 0.2
-                        }
-                    },
-                    {
-                        source: 'Node 2',
-                        target: 'Node 1',
-                        label: {
-                            show: true
-                        },
-                        lineStyle: {
-                            curveness: 0.2
-                        }
-                    },
-                    {
-                        source: 'Node 1',
-                        target: 'Node 3'
-                    },
-                    {
-                        source: 'Node 2',
-                        target: 'Node 3'
-                    },
-                    {
-                        source: 'Node 2',
-                        target: 'Node 4'
-                    },
-                    {
-                        source: 'Node 1',
-                        target: 'Node 4'
-                    }
-                ],
+                data: data,
+                links: links,
                 lineStyle: {
                     opacity: 0.9,
                     width: 2,
