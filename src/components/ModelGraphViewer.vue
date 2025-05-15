bf4854e5162212011a49000fe2df246f793826e7
cloudblaze123 <cloudblaze123@gmail.com>
2025-05-15 11:26:14 +0800

    【模型图表查看器】组件可显示两层全连接节点的连接关系和权重

--- src/components/ModelGraphViewer.vue
+++ src/components/ModelGraphViewer.vue
@@ -7,78 +7,136 @@ import { onMounted, onUnmounted, ref } from 'vue';
 import * as echarts from 'echarts';
 
 
+const weights = ref([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [10, 11, 12]
+])
+
+
+// // 定义数据
+// const data = [
+//     {
+//         name: 'Node 1',
+//         x: 300,
+//         y: 300
+//     },
+//     {
+//         name: 'Node 2',
+//         x: 800,
+//         y: 300
+//     },
+//     {
+//         name: 'Node 3',
+//         x: 550,
+//         y: 100
+//     },
+//     {
+//         name: 'Node 4',
+//         x: 550,
+//         y: 500
+//     }
+// ];
+
+
+// const links = [
+//     {
+//         source: 0,
+//         target: 1,
+//         symbolSize: [5, 20],
+//         label: {
+//             show: true
+//         },
+//         lineStyle: {
+//             width: 5,
+//             curveness: 0.2
+//         }
+//     },
+//     {
+//         source: 'Node 2',
+//         target: 'Node 1',
+//         label: {
+//             show: true
+//         },
+//         lineStyle: {
+//             curveness: 0.2
+//         }
+//     },
+//     {
+//         source: 'Node 1',
+//         target: 'Node 3'
+//     },
+//     {
+//         source: 'Node 2',
+//         target: 'Node 3'
+//     },
+//     {
+//         source: 'Node 2',
+//         target: 'Node 4'
+//     },
+//     {
+//         source: 'Node 1',
+//         target: 'Node 4'
+//     }
+// ]
+const data:Object[] = []
+const links:Object[] = []
+
+
+function prepareData() {
+    const inputNodes = []
+    const outputNodes = []
+    const inputLinks:Array<object[]> = []
+
+    inputNodes.length = weights.value[0].length
+    outputNodes.length = weights.value.length
+    inputLinks.length = outputNodes.length
+    inputLinks.fill([])
+
+    for (let i = 0; i < inputNodes.length; i++) {
+        inputNodes[i] = {
+            name: `Input ${i}`,
+            x: 100,
+            y: i * 100 + 100
+        }
+    }
+    for (let i = 0; i < outputNodes.length; i++) {
+        outputNodes[i] = {
+            name: `Output ${i}`,
+            x: 300,
+            y: i * 100 + 100
+        }
+    }
+    for (let i = 0; i < inputNodes.length; i++) {
+        for (let j = 0; j < outputNodes.length; j++) {
+            inputLinks[i].push({
+                source: `Input ${i}`,
+                target: `Output ${j}`,
+                label: {
+                    show: true,
+                    formatter: `${weights.value[j][i]}`
+                }
+            })
+        }
+    }
+
+    data.push(...inputNodes, ...outputNodes)
+    for (let i = 0; i < inputLinks.length; i++) {
+        links.push(...inputLinks[i])
+    }
+}
+prepareData()
+
+console.log(data)
+console.log(links)
+
+
+
+
 // 定义一个 ref 来引用 DOM 元素
 const chartRef = ref<HTMLDivElement | null>(null);
 let chartInstance: echarts.ECharts | null = null;
-
-
-// 定义数据
-const data = [
-    {
-        name: 'Node 1',
-        x: 300,
-        y: 300
-    },
-    {
-        name: 'Node 2',
-        x: 800,
-        y: 300
-    },
-    {
-        name: 'Node 3',
-        x: 550,
-        y: 100
-    },
-    {
-        name: 'Node 4',
-        x: 550,
-        y: 500
-    }
-]
-
-const links = [
-    {
-        source: 0,
-        target: 1,
-        symbolSize: [5, 20],
-        label: {
-            show: true
-        },
-        lineStyle: {
-            width: 5,
-            curveness: 0.2
-        }
-    },
-    {
-        source: 'Node 2',
-        target: 'Node 1',
-        label: {
-            show: true
-        },
-        lineStyle: {
-            curveness: 0.2
-        }
-    },
-    {
-        source: 'Node 1',
-        target: 'Node 3'
-    },
-    {
-        source: 'Node 2',
-        target: 'Node 3'
-    },
-    {
-        source: 'Node 2',
-        target: 'Node 4'
-    },
-    {
-        source: 'Node 1',
-        target: 'Node 4'
-    }
-]
-
-
-
 
 type EChartsOption = echarts.EChartsOption;
 
