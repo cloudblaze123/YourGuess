31c14c50f05d8af35074c811ad1d0cd1cca4bdb8
cloudblaze123 <cloudblaze123@gmail.com>
2025-05-29 16:07:49 +0800

    新增【图表配置项生成】脚本，【GameRecordPlayer】组件改为通过【图表配置项生成】脚本创建初始的图表配置

---
+++ src/charts/game-chart.ts
@@ -0,0 +1,136 @@
+import { Result } from '@/game/game';
+import type { ECharts, EChartsOption } from 'echarts';
+import { getResultMessage } from '@/game/game';
+
+
+
+
+function generateChartOption(max: number, min: number) {
+    const option:EChartsOption = {
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        xAxis: [
+            {
+                name: '次数',
+                type: 'category',
+
+                data : [],
+
+                axisLabel: {
+                    show: true,
+                    formatter: '{value}'
+                },
+                axisTick: {
+                    alignWithLabel: true,
+                },
+                splitLine: {
+                    show: false,
+                },
+            }
+        ],
+        yAxis: [
+            {
+                type: 'value',
+                minInterval: 1, // 最小间隔为整数
+
+                min: min,
+                max: max,
+
+                axisPointer: {
+                    show: true,
+                    type: 'line',
+                    label: {
+                        formatter: (params: any) => {
+                            const value = Math.floor(Number(params.value) + 0.5);
+                            return value.toString();
+                        }
+                    }
+                },
+                splitLine: {
+                    show: false,
+                },
+            }
+        ],
+        series: [
+            {
+                name: 'Guess',
+                type: 'bar',
+                label: {
+                    show: true,
+                    position: 'top',
+                    formatter: (params: any) => {
+                        return `${params.data.value}\n${getResultMessage(params.data.result)}`
+                    }
+                },
+                itemStyle: {
+                    color: (params: any) => {
+                        const result = params.data.result;
+                        if (result === Result.CORRECT) {
+                            return '#33d07c' // 绿色
+                        } else if (result === Result.TOO_SMALL) {
+                            return '#d03737' // 红色
+                        } else if (result === Result.TOO_BIG) {
+                            return '#2b80d0' //蓝色
+                        } else {
+                            return 'black' //不应该有这个颜色的情况，有则数据有误
+                        }
+                    },
+                    borderRadius: [3, 3, 0, 0],
+                },
+                barWidth: '95%',
+                
+                animationDelay: (idx) => {
+                    return idx * 100;
+                },
+                
+                animationDuration: 50,
+            },
+        ],
+        dataZoom: [
+            {
+                type: 'inside',
+                xAxisIndex: 0,
+            },
+            {
+                type: 'slider',
+                yAxisIndex: 0,
+                filterMode: 'none',
+            }
+        ],
+    };
+    
+    return option;
+}
+
+
+function updateChart(chart: ECharts, guessData: number[], resultData: Result[]) {
+    const xAxisData = Array(guessData.length + 5)
+        .fill(0)
+        .map((_, i) => i+1)
+    const xAxis = [
+        {
+            data: xAxisData
+        }
+    ]
+
+    chart.setOption({
+        xAxis: xAxis,
+        series: [
+            {
+                data: guessData.map((value, index) => {
+                    return {
+                        value: value,
+                        result: resultData[index]
+                    }
+                })
+            }
+        ]
+    });
+}
+
+
+export { generateChartOption, updateChart }
--- src/components/GameRecordPlayer.vue
+++ src/components/GameRecordPlayer.vue
@@ -6,7 +6,6 @@
 
 <script setup lang="ts">
 import { ref, onMounted, onUnmounted } from 'vue';
-import { Result, getResultMessage } from '@/game/game'
 import { GameRecord } from '@/game/game-record'
 
 
@@ -28,25 +27,7 @@ function play() {
 
 
 import * as echarts from 'echarts';
-
-
-const data: any[] = []
-
-function prepareData() {
-    data.length = 0;
-
-    const history = props.record.history;
-    for (let i = 0; i < history.length; i++) {
-        data.push({
-            x: i+1,
-            y: history[i].guess,
-            label: history[i].guess + '\n' + getResultMessage(history[i].result),
-            result: history[i].result,
-        });
-    }
-}
-
-
+import { generateChartOption, updateChart } from '@/charts/game-chart'
 
 
 // 定义一个 ref 来引用 DOM 元素
@@ -55,143 +36,26 @@ let chartInstance: echarts.ECharts | null = null;
 
 
 function refreshChart() {
-    prepareData();
-    
-    const xAxisData = data.map(item => item.x)
-    const rightSpaceData = [1,2,3,4,5].map(item => item + xAxisData.length)
-    xAxisData.push(...rightSpaceData)
-    const xAxis = [
-        {
-            data: xAxisData
-        }
-    ]
-    const yAxis = [
-        {
-            min: props.record.min - 1,
-            max: props.record.max + 1,
-        }
-    ]
-
-
-    if (chartInstance) {
-        chartInstance.setOption({
-            xAxis: xAxis,
-            yAxis: yAxis,
-            series: [
-                {
-                    data: data.map(item => item.y),
-                }
-            ]
-        });
+    if (!chartInstance) {
+        console.log('chartInstance is null')
+        return
     }
+
+    const history = props.record.history
+    const guessData = history.map(item => item.guess)
+    const resultData = history.map(item => item.result)
+    
+    updateChart(chartInstance, guessData, resultData)
 }
 
 
 
-
-type EChartsOption = echarts.EChartsOption;
 
 function initChart() {
     chartInstance = echarts.init(chartRef.value);
-    var option: EChartsOption;
-
-
-    option = {
-        grid: {
-            left: '3%',
-            right: '4%',
-            bottom: '3%',
-            containLabel: true
-        },
-        xAxis: [
-            {
-                name: '次数',
-                type: 'category',
-                data: data.map(item => item.x),
-                axisLabel: {
-                    show: true,
-                    formatter: '{value}'
-                },
-                axisTick: {
-                    alignWithLabel: true,
-                },
-                splitLine: {
-                    show: false,
-                },
-            }
-        ],
-        yAxis: [
-            {
-                type: 'value',
-                minInterval: 1, // 最小间隔为整数
-                axisPointer: {
-                    show: true,
-                    type: 'line',
-                    label: {
-                        formatter: (params) => {
-                            const value = Math.floor(Number(params.value) + 0.5);
-                            return value.toString();
-                        }
-                    }
-                },
-                splitLine: {
-                    show: false,
-                },
-            }
-        ],
-        series: [
-            {
-                name: 'Guess',
-                type: 'bar',
-                label: {
-                    show: true,
-                    position: 'top',
-                    formatter: (params) => {
-                        return `${data[params.dataIndex].label}`
-                    }
-                },
-                itemStyle: {
-                    color: (params: any) => {
-                        const result = data[params.dataIndex].result;
-                        if (result === Result.CORRECT) {
-                            return '#33d07c' // 绿色
-                        } else if (result === Result.TOO_SMALL) {
-                            return '#d03737' // 红色
-                        } else if (result === Result.TOO_BIG) {
-                            return '#2b80d0' //蓝色
-                        } else {
-                            return 'black' //不应该有这个颜色的情况，有则数据有误
-                        }
-                    },
-                    borderRadius: [3, 3, 0, 0],
-                },
-                barWidth: '95%',
-
-                animationDuration: 50,
-            },
-        ],
-        dataZoom: [
-            {
-                type: 'inside',
-                xAxisIndex: 0,
-            },
-            {
-                type: 'slider',
-                yAxisIndex: 0,
-                filterMode: 'none',
-            }
-        ],
-    };
-
-
+    const option = generateChartOption(props.record.max, props.record.min);
     option && chartInstance.setOption(option);
-
-
-    refreshChart();
 }
-
-
-
 
 
 
