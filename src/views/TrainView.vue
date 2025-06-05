2f681c139900d9a78ff0b53bccea3d3bbccf3799
cloudblaze123 <cloudblaze123@gmail.com>
2025-06-05 20:40:21 +0800

    【TrainView】支持使用多条数据进行训练

--- src/views/TrainView.vue
+++ src/views/TrainView.vue
@@ -4,10 +4,10 @@
     <ModelGraphViewer ref="modelGraphViewer" />
 
     <div class="flex flex-col mt-8">
-        <div>请输入用空格分隔的【输入值】（偏置节点无需输入）</div>
-        <input type="text" v-model="inputsNumbers" placeholder="用空格分隔的输入值形如 0.9 0.1 0.8">
-        <div>请输入用空格分隔的【目标值】（1到0之间）</div>
-        <input type="text" v-model="targetNumbers" placeholder="用空格分隔的目标值 形如 1 0">
+        <div>【输入值】与【目标值】（1到0之间）之间用 > 分割</div>
+        <div>多个值之间用空格分隔</div>
+        <div>形如：0.1 0.2 0.3 > 1 0</div>
+        <textarea v-model="trainData" rows="10" class="border-2"></textarea>
         <div>训练次数</div>
         <input type="number" v-model="trainTimes" placeholder="训练次数">
         <button @click="trainModel" class="btn btn-primary">训练模型</button>
@@ -33,11 +33,10 @@ import { ref } from 'vue'
 import ModelList from '@/components/model/ModelList.vue'
 
 
-const inputsNumbers = ref('0.9 0.1 0.8')
-const targetNumbers = ref('1.0 0.0')
+const trainData = ref('0.1 0.2 0.3 > 1 0 \n0.3 0.2 0.1 > 0 1')
 const trainTimes = ref(100)
 
-const forwardInput = ref('0.9 0.1 0.8')
+const forwardInput = ref('0.1 0.2 0.3')
 const forwardResults = ref('')
 
 
@@ -51,12 +50,19 @@ let nn = new NeuralNetwork([3, 4, 2]);
 
 function trainModel() {
     // 示例数据
-    const input = inputsNumbers.value.split(/\s+/).map(Number);
-    const target = targetNumbers.value.split(/\s+/).map(Number);
-
+    const dataStrArr = trainData.value.split('\n');
+    const dataArr = dataStrArr.map((str) => {
+        const [inputStr, targetStr] = str.split('>');
+        const input = inputStr.match(/-?\d+(\.\d+)?/g)!.map(Number);
+        const target = targetStr.match(/-?\d+(\.\d+)?/g)!.map(Number);
+        return { input, target };
+    });
+    
     // 训练神经网络
     for (let i = 0; i < trainTimes.value; i++) {
-        nn.train(input, target);
+        for (const { input, target } of dataArr) {
+            nn.train(input, target);
+        }
     }
 
     updateChart();
