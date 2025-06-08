3aade97d3f2e0ede6ab44a37fce3383da4e8d67d
cloudblaze123 <cloudblaze123@gmail.com>
2025-06-08 13:39:01 +0800

    优化【ModelView】视图的布局

--- src/views/ModelView.vue
+++ src/views/ModelView.vue
@@ -1,46 +1,70 @@
 <template>
-    <ModelViewer ref="modelViewerRef" />
-    
-    <ModelList @select-model="onSelectModel" class="mt-8"/>
+    <div class="lg:flex">
+        <div class="w-64">
+            <!-- sidebar header -->
+            <div class="flex space-x-2">
+                <h2 class="text-2xl mb-4">模型列表</h2>
+                <button @click="navigation ='model-add'" class="btn btn-outline">添加</button>
+            </div>
 
-    <div class="flex flex-col mb-16">
-        <div>创建模型</div>
-        <div>用空格分隔的每层节点数，形如：2 4 3 1</div>
-        <input type="text" v-model="networkFormatStr" placeholder="用单个空格分隔的每层节点数，形如：2 4 3 1" />
-        <button @click="createModel" class="btn btn-primary">创建模型</button>
-    </div>
+            <ModelList @select-model="onSelectModel" class="mt-8"/>
+        </div>
+        
+        <div class="w-full lg:w-0 lg:flex-1">
+            <div v-show="navigation ==='model-add'">
+                <div class="flex flex-col mb-16">
+                    <div>创建模型</div>
+                    <div>用空格分隔的每层节点数，形如：2 4 3 1</div>
+                    <input type="text" v-model="networkFormatStr" placeholder="用单个空格分隔的每层节点数，形如：2 4 3 1" />
+                    <button @click="createModel" class="btn btn-primary">创建模型</button>
+                </div>
+                
+                <div class="flex flex-col mt-16">
+                    <div class="flex flex-col">
+                        <div>加载模型：</div>
+                        <textarea v-model="modelJsonStr" placeholder="要加载的模型的JSON数据"></textarea>
+                        <button @click="loadModel" class="btn btn-primary">加载模型</button>
+                    </div>
+                </div>
+            </div>
 
-    <div class="flex flex-col mt-16">
-        <div class="flex flex-col">
-            <div>加载模型：</div>
-            <textarea v-model="modelJsonStr" placeholder="要加载的模型的JSON数据"></textarea>
-            <button @click="loadModel" class="btn btn-primary">加载模型</button>
+            <div v-show="navigation ==='model-view'">
+                <h2 class="text-2xl mb-4 font-bold">{{ modelName }}</h2>
+                <ModelViewer ref="modelViewerRef" />
+            </div>
         </div>
     </div>
 </template>
 
 
 <script setup lang="ts">
+import { ref } from 'vue'
+
 import ModelViewer from '@/components/model/ModelViewer.vue'
 
 import ModelList from '@/components/model/ModelList.vue'
 
+type NavigationItem = 'model-add' | 'model-view'
+
+const navigation = ref<NavigationItem>('model-add')
 
 
 
-import { ref } from 'vue'
+
 import { useModelStore } from '@/stores/model';
 const modelStore = useModelStore();
 
 
 const modelViewerRef = ref<any>()
 
+const modelName = ref('')
 
 
-
-function onSelectModel(modelName: string) {
-    console.log('onSelectModel', modelName)
-    modelViewerRef.value!.setModel(modelStore.getModel(modelName))
+function onSelectModel(propModelName: string) {
+    console.log('onSelectModel', propModelName)
+    navigation.value ='model-view'
+    modelName.value = propModelName
+    modelViewerRef.value!.setModel(modelStore.getModel(propModelName))
 }
 
 
